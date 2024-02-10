07a582a39fdc1e4681480121931f262a5006c715
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-10 12:47:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ SUDO_USERS = [6762113050, 6919199044]
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SHIVANSH475/SHIVANSH-MUSIC",
+    "https://github.com/SHIVANSH475/SHUKLA-MUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
