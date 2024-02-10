99d01300fafb0ca8575db9a68db37487e4eb4bb2
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-10 12:19:14 +0530

    Add files via upload

--- SHUKLAMUSIC/platforms/Apple.py
+++ SHUKLAMUSIC/platforms/Apple.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 from youtubesearchpython.__future__ import VideosSearch
 
 
-class RessoAPI:
+class AppleAPI:
     def __init__(self):
-        self.regex = r"^(https:\/\/m.resso.com\/)(.*)$"
-        self.base = "https://m.resso.com/"
+        self.regex = r"^(https:\/\/music.apple.com\/)(.*)$"
+        self.base = "https://music.apple.com/in/playlist/"
 
     async def valid(self, link: str):
         if re.search(self.regex, link):
@@ -26,18 +26,13 @@ class RessoAPI:
                     return False
                 html = await response.text()
         soup = BeautifulSoup(html, "html.parser")
+        search = None
         for tag in soup.find_all("meta"):
             if tag.get("property", None) == "og:title":
-                title = tag.get("content", None)
-            if tag.get("property", None) == "og:description":
-                des = tag.get("content", None)
-                try:
-                    des = des.split("·")[0]
-                except:
-                    pass
-        if des == "":
-            return
-        results = VideosSearch(title, limit=1)
+                search = tag.get("content", None)
+        if search is None:
+            return False
+        results = VideosSearch(search, limit=1)
         for result in (await results.next())["result"]:
             title = result["title"]
             ytlink = result["link"]
@@ -52,3 +47,25 @@ class RessoAPI:
             "thumb": thumbnail,
         }
         return track_details, vidid
+
+    async def playlist(self, url, playid: Union[bool, str] = None):
+        if playid:
+            url = self.base + url
+        playlist_id = url.split("playlist/")[1]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status != 200:
+                    return False
+                html = await response.text()
+        soup = BeautifulSoup(html, "html.parser")
+        applelinks = soup.find_all("meta", attrs={"property": "music:song"})
+        results = []
+        for item in applelinks:
+            try:
+                xx = (((item["content"]).split("album/")[1]).split("/")[0]).replace(
+                    "-", " "
+                )
+            except:
+                xx = ((item["content"]).split("album/")[1]).split("/")[0]
+            results.append(xx)
+        return results, playlist_id
