39ac44340fe72c92c68a7ee535630161542e6248
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-10 12:43:09 +0530

    Add files via upload

---
+++ SHUKLAMUSIC/utils/inline/help.py
@@ -0,0 +1,129 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from SHUKLAMUSIC import app
+
+
+def help_pannel(_, START: Union[bool, int] = None):
+    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
+        InlineKeyboardButton(
+            text="★ ᴍᴏʀᴇ ★", callback_data="help_callback hb13"
+        ),
+    ]
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="⚡ ᴀᴅᴍɪɴ ⚡",
+                    callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text="⚡ᴀᴜᴛʜ⚡",
+                    callback_data="help_callback hb2",
+                ),
+            
+            ],
+            [
+                InlineKeyboardButton(
+                    text="⚡ ʙʟᴏᴄᴋ ⚡",
+                    callback_data="help_callback hb3",
+                ),
+                InlineKeyboardButton(
+                    text="⚡ ɢᴄᴀsᴛ ⚡",
+                    callback_data="help_callback hb4",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="⚡ ɢʙᴀɴ ⚡",
+                    callback_data="help_callback hb12",
+                ),
+                InlineKeyboardButton(
+                    text="⚡ ʟʏʀɪᴄs ⚡",
+                    callback_data="help_callback hb5",
+                ),
+            ],
+                        
+            [
+                InlineKeyboardButton(
+                    text="⚡ ᴘʟᴀʏʟɪsᴛ ⚡",
+                    callback_data="help_callback hb6",
+                ),
+                InlineKeyboardButton(
+                    text="⚡ ᴠᴏɪᴄᴇ-ᴄʜᴀᴛ ⚡",
+                    callback_data="help_callback hb10",
+                ),
+            ],
+            [
+                            InlineKeyboardButton(text="⚡𝐒ʜɪᴠᴀɴsʜ-𝐗ᴅ⚡️", url=f"https://t.me/SHIVANSH39"),
+                            InlineKeyboardButton(text="⚡️ sʜᴜᴋʟᴀ ⚡️", url=f"https://t.me/ITSZ_SHIVANSH"),
+            ],
+            [
+           
+                InlineKeyboardButton(
+                    text="⚡ ᴘʟᴀʏ ⚡",
+                    callback_data="help_callback hb8",
+                ),
+            
+            
+                InlineKeyboardButton(
+                    text="⚡ sᴜᴅᴏ ⚡",
+                    callback_data="help_callback hb9",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="⚡ sᴛᴀʀᴛ ⚡",
+                    callback_data="help_callback hb11",
+                ),
+            ],
+            mark,
+        ]
+    )
+    return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"close"
+                ),
+                InlineKeyboardButton(
+                    text="★ ᴍᴏʀᴇ ★", callback_data="help_callback hb14"
+                )
+
+            ]
+        ]
+    )
+    return upl
+
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="⚡ ʜᴇʟᴘ ⚡",
+                callback_data="settings_back_helper",
+            ),
+        ],
+    ]
+    return buttons
+    
+    
+    
--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -1,1 +1,73 @@
+from typing import Union
 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def queue_markup(
+    _,
+    DURATION,
+    CPLAY,
+    videoid,
+    played: Union[bool, int] = None,
+    dur: Union[bool, int] = None,
+):
+    not_dur = [
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_1"],
+                callback_data=f"GetQueued {CPLAY}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ]
+    ]
+    dur = [
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_2"].format(played, dur),
+                callback_data="GetTimer",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["QU_B_1"],
+                callback_data=f"GetQueued {CPLAY}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ],
+    ]
+    upl = InlineKeyboardMarkup(not_dur if DURATION == "Unknown" else dur)
+    return upl
+
+
+def queue_back_markup(_, CPLAY):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"queue_back_timer {CPLAY}",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ]
+        ]
+    )
+    return upl
+
+
+def aq_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(text="𐏓 ⃪⃝𝐒ʜɪᴠᴀɴsʜ-𝐗ᴅ", url="https://t.me/SHIVANSH39"),
+            InlineKeyboardButton(text="⎯꯭𝐒ʜᴜᴋʟᴀ", url="https://t.me/ITSZ_SHIVANSH"),
+        ],
+    ]
+    return buttons
